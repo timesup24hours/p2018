64fa600f9a79c0a7c0d24625ba8d396cccd807d0
kk <kk@kk.PK5001Z>
2018-07-30 21:38:14 -0500

    change css on css_effect page

--- src/components/AppNavBar/AppNavBar.js
+++ src/components/AppNavBar/AppNavBar.js
@@ -31,26 +31,28 @@ class AppNavBar extends Component {
           <nav>
             <ul className={styles.nav_ul}>
               <li className={styles.nav_ul_li_1}>
-                <Link to="/p2018/home">Home</Link>
+                <Link to="/home">Home</Link>
               </li>
               <li className={styles.nav_ul_li_2}>
-                <Link to="/p2018/my_notes">My notes</Link>
+                <Link to="/my_notes">My notes</Link>
               </li>
               <li className={styles.nav_ul_li_3}>
-                <Link to="/p2018/css_effects">css effect</Link>
+                <Link to="/css_effects">css effect</Link>
               </li>
               <li className={styles.nav_ul_li_4}>
-                <Link to="/p2018/contact_info">contact info</Link>
+                <Link to="/contact_info">contact info</Link>
               </li>
             </ul>
           </nav>
           <div>
-            <Route exact path="/p2018/home" component={Home} />
-            <Route path="/p2018/my_notes" component={MyNotes} />
-            <Route path="/p2018/css_effects" component={CssEffects} />
-            <Route path="/p2018/contact_info" component={ContactInfo} />
-            <Route path="*" component={() => <div>404</div>} />
-            <Redirect from="/" to="/p2018/home" />
+            <Switch>
+              <Route exact path="/home" component={Home} />
+              <Route path="/my_notes" component={MyNotes} />
+              <Route path="/css_effects" component={CssEffects} />
+              <Route path="/contact_info" component={ContactInfo} />
+              <Route path="*" component={() => <div>404</div>} />
+              <Redirect from="/" to="/home" />
+            </Switch>
           </div>
         </div>
       </Router>
