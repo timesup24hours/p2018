bb3546ff7125e3fe71d8589ac567a28430c33fcc
kk <kk@kk.PK5001Z>
2018-08-07 21:20:20 -0500

    fixed the Link in cssPage

--- src/components/GlowingIconhoverEffect/GlowingIconhoverEffect.js
+++ src/components/GlowingIconhoverEffect/GlowingIconhoverEffect.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink as Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styles from './index.css';
 
 export default () => {
@@ -7,17 +7,17 @@ export default () => {
     <div className={styles.container}>
       <ul className={styles.ul}>
         <li className={styles.li}>
-          <Link href="/css_effects">
+          <Link to="/css_effects">
             <i className="fas fa-archway" />
           </Link>
         </li>
         <li className={styles.li}>
-          <Link href="/css_effects">
+          <Link to="/css_effects">
             <i className="fas fa-award" />
           </Link>
         </li>
         <li className={styles.li}>
-          <Link href="/css_effects">
+          <Link to="/css_effects">
             <i className="fas fa-apple-alt" />
           </Link>
         </li>
