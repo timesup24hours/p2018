b319d8770e909c5571ef0e4e531dad925c3424c6
kk <kk@kk.local>
2019-07-17 12:54:50 -0700

    test CssButtonBorder component

---
+++ src/components/CssButtonBorder/__tests__/CssButtonBorder.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+import { CssButtonBorder } from '../CssButtonBorder';
+
+describe('MyNotes.test', () => {
+  it('should render correctly in "debug" mode', () => {
+    const component = shallow(<CssButtonBorder />);
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should be rendered Looding component', () => {
+    const wrapper = shallow(<CssButtonBorder />);
+    expect(wrapper.contains('Click Me')).toBeTruthy();
+  });
+
+  it('should be click the button', () => {
+    const push = jest.fn();
+    const wrapper = mount(<CssButtonBorder />);
+    wrapper.setProps({ history: { push } });
+
+    wrapper.find('[data-testid="click"]').simulate('click');
+    expect(push).toBeCalled();
+  });
+});
